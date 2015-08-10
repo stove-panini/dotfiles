53ae201c107242f9455eace5d218e23bbb1e94d0
stove-panini <s.panicho@gmail.com>
2015-08-09 21:46:02 -0400

    check if dir is present but empty

--- i3-desktop/.local/share/i3/df-ext.sh
+++ i3-desktop/.local/share/i3/df-ext.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-if [ -d /media/External ]; then
+#
+# Display usage stats for external drives mounted w/ udevil
+
+# First, it tests if the directory is present but empty
+# (leftover when unmounted on shutdown rather than by udevil)
+
+if [ ! -f /media/External/* ]; then
+    echo "EXT --"
+elif [ -d /media/External ]; then
     df -h | grep "External" | awk '{print "EXT "$4,"["$5"]"}'
 else
     echo "EXT --"
