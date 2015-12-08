86846022623e1669a9c555eeb74d9cc95b4c214b
Stephen P <s.panicho@gmail.com>
2015-12-07 23:26:18 -0500

    touchpad script / xss lock

--- i3-laptop/.local/share/i3/touchpad_toggle.sh
+++ i3-laptop/.local/share/i3/touchpad_toggle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if synclient -l | grep "TouchpadOff .*=.*0" ; then
-    synclient TouchpadOff=1 ;
+    synclient TouchpadOff=1 && xinput disable "SynPS/2 Synaptics TouchPad";
 else
-    synclient TouchpadOff=0 ;
+    synclient TouchpadOff=0 && xinput enable "SynPS/2 Synaptics TouchPad";
 fi
