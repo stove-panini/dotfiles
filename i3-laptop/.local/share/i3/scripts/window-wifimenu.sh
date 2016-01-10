69da5a9f92e6db3de5ad22fb00cf7c5492492609
stove-panini <s.panicho@gmail.com>
2016-01-10 11:16:11 -0500

    aaand back to urxvt

--- i3-laptop/.local/share/i3/scripts/window-wifimenu.sh
+++ i3-laptop/.local/share/i3/scripts/window-wifimenu.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-i3-msg exec "termite --name=wifi-menu --exec='sudo wifi-menu -o'"
+if [[ $TERM == rxvt-unicode* ]]; then
+    i3-msg exec "urxvt -name 'wifi-menu' -e 'sudo wifi-menu -o'"
+
+elif [[ $TERM == xterm-termite ]]; then
+    i3-msg exec "termite --name=wifi-menu --exec='sudo wifi-menu -o'"
+fi
