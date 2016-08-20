aea16804db7991fd30325e06c9756291bd73b317
stove-panini <s.panicho@gmail.com>
2016-08-20 02:46:45 -0400

    thermal zone woes

--- tmux/.tmux/scripts/tmuxline-cpu.sh
+++ tmux/.tmux/scripts/tmuxline-cpu.sh
@@ -3,7 +3,10 @@
 # ~/.tmux/scripts/tmuxline-cpu.sh: Reports CPU usage as a percentage
 #
 
-# TODO: use awk for floating point operations instead of dropping decimals with sed
+# TODO: 1. use awk for floating point operations instead of dropping decimals with sed
+#       2. magically determine the right thermal_zone ugh
+
+
 idle=$(mpstat | tail -n1 | awk '{print $13}' | sed s/\...$//)
 temp=$(cat /sys/class/thermal/thermal_zone2/temp | cut -c 1-2)
 
