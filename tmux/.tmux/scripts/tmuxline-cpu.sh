7082c79c520a0ae681b7088a99a79670c04c0779
stove-panini <s.panicho@gmail.com>
2016-08-20 02:41:19 -0400

    change variable name

--- tmux/.tmux/scripts/tmuxline-cpu.sh
+++ tmux/.tmux/scripts/tmuxline-cpu.sh
@@ -4,12 +4,12 @@
 #
 
 # TODO: use awk for floating point operations instead of dropping decimals with sed
-cusage=$(mpstat | tail -n1 | awk '{print $13}' | sed s/\...$//)
-ctemp=$(cat /sys/class/thermal/thermal_zone2/temp | cut -c 1-2)
+idle=$(mpstat | tail -n1 | awk '{print $13}' | sed s/\...$//)
+temp=$(cat /sys/class/thermal/thermal_zone2/temp | cut -c 1-2)
 
 if
     ! command -v mpstat >/dev/null; then
         echo "Install sysstat package!"
 else
-    echo "CPU $(( 100 - $cusage ))% | $ctemp°C"
+    echo "CPU $(( 100 - $idle ))% | $temp°C"
 fi
