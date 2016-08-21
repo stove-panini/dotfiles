c4e2909d13a193c529b42d9747effe07ea6c36cc
stove-panini <s.panicho@gmail.com>
2016-08-21 04:33:29 -0400

    thermal zone prediction via shameful magic

--- tmux/.tmux/scripts/tmuxline-cpu.sh
+++ tmux/.tmux/scripts/tmuxline-cpu.sh
@@ -3,10 +3,12 @@
 # ~/.tmux/scripts/tmuxline-cpu.sh: Reports CPU usage as a percentage
 #
 
-# TODO: magically determine the right thermal_zone ugh
+# TODO: pray to god that the highest thermal_zoneN dir is reserved for cpu avg
+#       temp across all machines everywhere ever (ha ha ha)
 
+zone=$(ls -1 /sys/class/thermal/ | grep "thermal_zone" | tail -1)
 idle=$(mpstat | tail -n1 | awk '{printf "%0.f", $13}')
-temp=$(cat /sys/class/thermal/thermal_zone2/temp | cut -c 1-2)
+temp=$(cat /sys/class/thermal/$zone/temp | cut -c 1-2)
 
 if
     ! command -v mpstat >/dev/null; then
