970528fe68195950f3ae101ef7057baa55d2a112
stove-panini <s.panicho@gmail.com>
2016-08-21 03:32:20 -0400

    Round those decimals and pad the output

--- tmux/.tmux/scripts/tmuxline-cpu.sh
+++ tmux/.tmux/scripts/tmuxline-cpu.sh
@@ -3,16 +3,16 @@
 # ~/.tmux/scripts/tmuxline-cpu.sh: Reports CPU usage as a percentage
 #
 
-# TODO: 1. use awk for floating point operations instead of dropping decimals with sed
-#       2. magically determine the right thermal_zone ugh
+# TODO: magically determine the right thermal_zone ugh
 
-
-idle=$(mpstat | tail -n1 | awk '{print $13}' | sed s/\...$//)
+idle=$(mpstat | tail -n1 | awk '{printf "%0.f", $13}')
 temp=$(cat /sys/class/thermal/thermal_zone2/temp | cut -c 1-2)
 
 if
     ! command -v mpstat >/dev/null; then
         echo "Install sysstat package!"
 else
-    echo "CPU $(( 100 - $idle ))% | $temp°C"
+    echo -n "CPU "
+    printf "%2s" "$(( 100 - $idle ))"
+    echo "% | $temp°C"
 fi
