945f5f662e6fb70e8663ac4160d3dad4cc4a7cc7
stove-panini <s.panicho@gmail.com>
2016-09-05 23:24:39 -0400

    actually use mpstat properly

--- tmux/.tmux/scripts/tmuxline-cpu.sh
+++ tmux/.tmux/scripts/tmuxline-cpu.sh
@@ -3,8 +3,10 @@
 # ~/.tmux/scripts/tmuxline-cpu.sh: Reports CPU usage and temperature
 #
 
-temp=$(sensors | grep "Physical id 0" | awk '{ sub(/\+/, ""); print $4}' | cut -c -2)
-idle=$(mpstat | tail -1 | awk '{printf "%0.f", $13}')
+temp=$(sensors | grep "Physical id 0" | awk '{ sub(/\+/, ""); print $4 }' | cut -c -2)
+# mpstat must collect multiple samples of cpu usage to generate accurate usage %
+# awk subtracts idle% from 100 then rounds decimals
+idle=$(mpstat 1 4 | tail -1 | awk '{ printf "%0.f", (100 - $12) }')
 
 if
     ! command -v mpstat >/dev/null; then
@@ -14,6 +16,7 @@ elif
         echo "Install lm_sensors"
 else
     echo -n "CPU "
-    printf "%2s" "$(( 100 - $idle ))"
+    # pad output to 2 spaces to prevent statusbar wiggle
+    printf "%2s" "$idle"
     echo "% | $temp°C"
 fi
