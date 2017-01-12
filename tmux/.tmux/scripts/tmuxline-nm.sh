b34343a630fd040c3feecb8a285c923e785b578c
stove-panini <s.panicho@gmail.com>
2017-01-12 07:44:32 -0500

    fix OR comparison

--- tmux/.tmux/scripts/tmuxline-nm.sh
+++ tmux/.tmux/scripts/tmuxline-nm.sh
@@ -12,12 +12,12 @@ W_SSID=$(nmcli -t --fields IN-USE,SSID dev wifi | grep "\*" | cut -d ":" -f 2)
 W_BARS=$(nmcli -t --fields IN-USE,BARS dev wifi | grep "\*" | cut -d ":" -f 2)
 
 
-if [[ $TYPE = ethernet || bridge ]]; then
+if [[ $TYPE = "ethernet" || $TYPE = "bridge" ]]; then
     # Ethernet output: ENO1 192.168.1.123
     echo "${DEVICE^^} $IP4"
-elif [[ $TYPE = wifi ]]; then
+elif [[ $TYPE = "wifi" ]]; then
     # Wifi output: HOME_NETWORK ▂▄▆_
-    echo "$W_SSID $W_BARS"
+    echo "$W_SSID $W_BARS ($IP4)"
 else
     echo "NET DOWN"
 fi
