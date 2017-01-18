c370ea0bcf3f3057d8acb659393c9467a3082131
stove-panini <s.panicho@gmail.com>
2017-01-17 19:27:29 -0500

    combine printf options

--- tmux/.tmux/scripts/cpu.sh
+++ tmux/.tmux/scripts/cpu.sh
@@ -6,7 +6,7 @@
 TEMP=$(sensors | grep "Physical id 0" | awk '{ printf "%d", $4 }')
 # mpstat must collect multiple samples of cpu usage to generate accurate usage %
 # awk subtracts idle% from 100 then rounds decimals
-IDLE=$(mpstat 1 4 | awk 'END { printf "%0.f", (100 - $12) }')
+USE=$(mpstat 1 4 | awk 'END { printf "%2.f", (100 - $12) }')
 
 if
     ! command -v mpstat >/dev/null; then
@@ -16,5 +16,5 @@ elif
         echo "Install lm_sensors"
 else
     # pad idle% to prevent wiggle
-    printf "CPU %2s%% / ${TEMP}°C" "${IDLE}"
+    echo "CPU ${USE}% / ${TEMP}°C"
 fi
