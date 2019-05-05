7f7145fed62eb4c2ab30425aeb5fb8b59b1e5324
stove-panini <s.panicho@gmail.com>
2019-05-05 01:22:34 +0000

    No more tmux

--- tmux/.tmux/scripts/mem.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# ~/.tmux/scripts/tmuxline-mem.sh: Displays free physical memory
-#
-
-FREE=$(free -h | awk 'NR==2 { print $4 }')
-PERCENT=$(free | awk 'NR==2 { printf "%.0f", ($3 / $2) * 100 }')
-
-echo "MEM ${FREE} / ${PERCENT}%"
--- tmux/.tmux/scripts/swp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# ~/.tmux/scripts/tmuxline-swap.sh: Displays swap usage
-#
-
-FREE=$(free -h | awk 'NR==3 { print $4 }')
-PERCENT=$(free | awk 'NR==3 {printf "%.0f", ($3 / $2) * 100 }')
-
-echo "SWP ${FREE} / ${PERCENT}%"
